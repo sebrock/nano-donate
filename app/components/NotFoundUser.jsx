1537d27fffe52e3f7849879bde2eb8c63bf7d21f
sebrock <156824+sebrock@users.noreply.github.com>
2020-07-05 14:35:56 +0200

    Updates to NotFoundUser
    Removed link from messages.json and placed link into component.

--- app/components/NotFoundUser.jsx
+++ app/components/NotFoundUser.jsx
@@ -4,6 +4,7 @@ const NotFoundUser = () => {
   return (
     <div className="styleOfError">
       {chrome.i18n.getMessage("err_notRegist")}
+	  <a href='https://github.com/sebrock/banano-donate/tree/banano-dev#how-can-i-receive-donations-through-bananodonate' target='_blank'>Click here</a>
     </div>
   );
 };
