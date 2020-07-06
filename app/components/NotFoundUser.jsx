e3122c2d716e0889af56fde6b8a29c2cc806df4c
sebrock <156824+sebrock@users.noreply.github.com>
2020-07-06 22:19:42 +0200

    Added messages.json
    Added messages.json for "en" and a copy of that to all languages as placeholder.
    Used locales in NotFoundUser.jsx

--- app/components/NotFoundUser.jsx
+++ app/components/NotFoundUser.jsx
@@ -4,8 +4,13 @@ const NotFoundUser = () => {
   return (
     <div className="styleOfError">
       {chrome.i18n.getMessage("err_notRegist")}
-	  <a href='https://github.com/sebrock/banano-donate/tree/banano-dev#how-can-i-receive-donations-through-bananodonate' target='_blank'>Click here</a>
-    </div>
+	  <a href='https://github.com/sebrock/banano-donate/tree/banano-dev#how-can-i-receive-donations-through-bananodonate' target='_blank'>{chrome.i18n.getMessage("msg_ClickHere")}</a></br></br>
+	  {chrome.i18n.getMessage("msg_NotRegist_NotOwner_1")}
+	  <a href='https://github.com/sebrock/banano-donate/tree/banano-dev#how-can-i-receive-donations-through-bananodonate' target='_blank'>{chrome.i18n.getMessage("msg_NotRegist_NotOwner_2")}</a>{chrome.i18n.getMessage("msg_NotRegist_NotOwner_3")}</br>
+	  <textarea rows="12" cols="52">
+	  {chrome.i18n.getMessage("msg_NotRegist_NotOwner_4")} <a href='https://github.com/sebrock/banano-donate/tree/banano-dev#how-can-i-receive-donations-through-bananodonate' target='_blank'>{chrome.i18n.getMessage("msg_ClickHere")}</a></br></br>
+	  {chrome.i18n.getMessage("msg_NotRegist_NotOwner_4")}</textarea>
+	  </div>
   );
 };
 
