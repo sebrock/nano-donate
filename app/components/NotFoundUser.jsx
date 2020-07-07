7b80320c0af4eef841b81ca6edb10e7bff7d9647
sebrock <156824+sebrock@users.noreply.github.com>
2020-07-07 11:53:50 +0200

    tried to fix NotFoundUser.jsx

--- app/components/NotFoundUser.jsx
+++ app/components/NotFoundUser.jsx
@@ -4,14 +4,21 @@ const NotFoundUser = () => {
   return (
     <div className="styleOfError">
       {chrome.i18n.getMessage("err_notRegist")}
-	  <a href='https://github.com/sebrock/banano-donate/tree/banano-dev#how-can-i-receive-donations-through-bananodonate' target='_blank'>{chrome.i18n.getMessage("msg_ClickHere")}</a></br></br>
-	  {chrome.i18n.getMessage("msg_NotRegist_NotOwner_1")}
-	  <a href='https://github.com/sebrock/banano-donate/tree/banano-dev#how-can-i-receive-donations-through-bananodonate' target='_blank'>{chrome.i18n.getMessage("msg_NotRegist_NotOwner_2")}</a>{chrome.i18n.getMessage("msg_NotRegist_NotOwner_3")}</br>
-	  <textarea rows="12" cols="52">
-	  {chrome.i18n.getMessage("msg_NotRegist_NotOwner_4")} <a href='https://github.com/sebrock/banano-donate/tree/banano-dev#how-can-i-receive-donations-through-bananodonate' target='_blank'>{chrome.i18n.getMessage("msg_ClickHere")}</a></br></br>
-	  {chrome.i18n.getMessage("msg_NotRegist_NotOwner_4")}</textarea>
-	  </div>
-  );
+	  <a href='https://github.com/sebrock/banano-donate/tree/banano-dev#how-can-i-receive-donations-through-bananodonate' target='_blank'>
+		{chrome.i18n.getMessage("msg_ClickHere")}
+	  </a>
+		  <br /><br />
+	   {chrome.i18n.getMessage("msg_NotRegist_NotOwner_1")}
+	  <a href='https://github.com/sebrock/banano-donate/tree/banano-dev#how-can-i-receive-donations-through-bananodonate' target='_blank'>{chrome.i18n.getMessage("msg_NotRegist_NotOwner_2")}
+	  </a>{chrome.i18n.getMessage("msg_NotRegist_NotOwner_3")}
+	  <br />	
+	  <textarea rows="12" cols="52" name="emailtext" readonly >
+		  {chrome.i18n.getMessage("msg_NotRegist_NotOwner_4")} 
+		  <a href='https://github.com/sebrock/banano-donate/tree/banano-dev#how-can-i-receive-donations-through-bananodonate' target='_blank'>{chrome.i18n.getMessage("msg_ClickHere")}</a><br /><br />
+	{chrome.i18n.getMessage("msg_NotRegist_NotOwner_5")}
+	  </textarea>
+	  	</div>
+	);
 };
 
 export default NotFoundUser;
